b789ec73b6efe0b5b434dfc5e1cbf62caf60a59b
Fujia Wu <fujiawu@gatech.edu>
2016-03-16 08:06:08 -0400

    Finished 1st version of Add Customer layout

---
+++ Project2/TCCart/app/src/main/java/tccar/seclass/gatech/edu/tccart/AddCustomerActivity.java
@@ -0,0 +1,13 @@
+package tccar.seclass.gatech.edu.tccart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddCustomerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_customer);
+    }
+}
