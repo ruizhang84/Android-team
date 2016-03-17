2dbfbd6cd4beab197c1da412bd39bec50d72ff6e
Fujia Wu <fujiawu@gatech.edu>
2016-03-17 08:03:05 -0400

    Added PrintCard and ViewTransaction activity layout, made minor changes in the main activity layout

---
+++ Project2/TCCart/app/src/main/java/tccar/seclass/gatech/edu/tccart/PrintCardActivity.java
@@ -0,0 +1,13 @@
+package tccar.seclass.gatech.edu.tccart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrintCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_print_card);
+    }
+}
