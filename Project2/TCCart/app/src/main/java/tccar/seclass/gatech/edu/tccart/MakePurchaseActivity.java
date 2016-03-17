6c3846a0255a34fa399175f7cd83e2302b5a9241
Fujia Wu <fujiawu@gatech.edu>
2016-03-17 08:06:46 -0400

    Started MakePurchase Activity layout

---
+++ Project2/TCCart/app/src/main/java/tccar/seclass/gatech/edu/tccart/MakePurchaseActivity.java
@@ -0,0 +1,13 @@
+package tccar.seclass.gatech.edu.tccart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MakePurchaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_purchase);
+    }
+}
