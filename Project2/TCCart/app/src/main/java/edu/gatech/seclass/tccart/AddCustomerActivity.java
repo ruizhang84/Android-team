b0ed8eb03e89d9242fac8be1f3c3f18139b06257
Fujia Wu <fujiawu@gatech.edu>
2016-03-18 20:08:20 -0400

    Completed handleAddCustomer function

--- Project2/TCCart/app/src/main/java/edu/gatech/seclass/tccart/AddCustomerActivity.java
+++ Project2/TCCart/app/src/main/java/edu/gatech/seclass/tccart/AddCustomerActivity.java
@@ -1,11 +1,13 @@
 package edu.gatech.seclass.tccart;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddCustomerActivity extends AppCompatActivity {
 
@@ -32,6 +34,41 @@ public class AddCustomerActivity extends AppCompatActivity {
     }
 
     public void handleAdd(View view){
+        String id = textID.getText().toString();
+        if (id.length() != 8){
+            Context context = getApplicationContext();
+            CharSequence text = "Not a valid ID!";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            return;
+        }
+        String name = editTextName.getText().toString();
+        String email = editTextEmail.getText().toString();
+        if (name.length() <= 0 || email.length() <= 0) {
+            Context context = getApplicationContext();
+            CharSequence text = "Name or email cannot be empty!";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            return;
+        }
+        Customer customer = new Customer(name, email, id);
+        if (Customer.customerMap.containsKey(id)){
+            Context context = getApplicationContext();
+            CharSequence text = "This id already exists! Please generate a new id!";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            return;
+        }
+        Customer.customerMap.put(id, customer);
+        Context context = getApplicationContext();
+        CharSequence text = "Customer is added successfully!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+        return;
     }
 
     public void handleClear(View view){
