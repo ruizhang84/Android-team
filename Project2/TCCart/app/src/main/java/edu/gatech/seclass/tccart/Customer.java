a0c0c9ea257edd0b5e8780625479c21662fa0bf8
Fujia Wu <fujiawu@gatech.edu>
2016-03-23 09:20:51 -0400

    Add an Calendar and Date conversion example

--- Project2/TCCart/app/src/main/java/edu/gatech/seclass/tccart/Customer.java
+++ Project2/TCCart/app/src/main/java/edu/gatech/seclass/tccart/Customer.java
@@ -82,3 +82,28 @@ public class Customer {
     }
 
 }
+
+
+/*
+
+import java.util.Date;
+import java.util.Calendar;
+
+public class HelloWorld{
+
+     public static void main(String []args){
+       
+       Date date = new Date(2121212333121L);
+       System.out.println(date.toString());
+       
+       Calendar cal = Calendar.getInstance();
+       cal.setTime(date);
+       System.out.println(cal.get(Calendar.YEAR));
+       System.out.println(cal.get(Calendar.MONTH));
+       System.out.println(cal.get(Calendar.DAY_OF_MONTH));
+       
+     }
+}
+
+
+*/
